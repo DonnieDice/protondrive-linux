110e6bb9ec8d1b86bc71e9233d4658544ea9c7d2
DonnieDice <donniedice@users.noreply.github.com>
2026-05-13 13:01:27 -0500

    Fix Snap WebKitGTK layout and wrapper YAML

--- packaging/snap/proton-drive-wrapper.sh
+++ packaging/snap/proton-drive-wrapper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+export LD_LIBRARY_PATH="$SNAP/lib:$SNAP/usr/lib:$SNAP/lib/x86_64-linux-gnu:$SNAP/usr/lib/x86_64-linux-gnu${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 export WEBKIT_DISABLE_DMABUF_RENDERER=1
 export WEBKIT_DISABLE_COMPOSITING_MODE=1
-export GDK_GL=software
-export WEBKIT_NETWORK_PROCESS_PATH="$SNAP/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1/WebKitNetworkProcess"
-export WEBKIT_WEB_PROCESS_PATH="$SNAP/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1/WebKitWebProcess"
-export WEBKIT_GPU_PROCESS_PATH="$SNAP/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1/WebKitGPUProcess"
+export WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1
 export WEBKIT_INJECTED_BUNDLE_PATH="$SNAP/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1/injected-bundle"
+export GDK_GL=disable
+export LIBGL_ALWAYS_SOFTWARE=1
+export GSK_RENDERER=cairo
 exec "$SNAP/usr/bin/proton-drive" "$@"
